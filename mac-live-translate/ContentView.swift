1869c77140bee0e0168d2aaf6e58d03e948bef4a
jerrypm <jerrypurnamamaulid@gmail.com>
2026-05-19 10:30:29 +0700

    Initial Commit

---
+++ mac-live-translate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mac-live-translate
+//
+//  Created by Jeri Purnama Maulid on 19/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mac-live-translate/mac_live_translateApp.swift
@@ -0,0 +1,17 @@
+//
+//  mac_live_translateApp.swift
+//  mac-live-translate
+//
+//  Created by Jeri Purnama Maulid on 19/05/26.
+//
+
+import SwiftUI
+
+@main
+struct mac_live_translateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
